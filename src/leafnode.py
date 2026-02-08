11debc1c7d67e39506a4bcd97d80684a683c971f
andreas <andre@LAPTOP-61UG31CH.localdomain>
2026-02-08 16:39:46 -0500

    Finished leaf node class and unit tests

--- src/leafnode.py
+++ src/leafnode.py
@@ -2,6 +2,27 @@ from htmlnode import HTMLNode
 
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props = None):
-        super.__init__(tag, value, props)
+        super().__init__(tag, value, props)
+        self.props = props
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("A value is required")
+        if self.tag is None:
+            return self.value
+        else:
+            if self.tag == "a":
+                result = f"<{self.tag} "
+                for k, v in self.props.items():
+                    result += f"{k}={v}"
+                result += f">{self.value}</{self.tag}>"
+                return result
+            elif self.tag == "p" or self.tag == "b" or self.tag == "i":
+                return f"<{self.tag}>{self.value}</{self.tag}>"
+            
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value},{self.props})"
+
 
     
+
