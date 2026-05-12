02a7607b8126a3b284e7ecde7a990e199ac321b4
andreas <andre@LAPTOP-61UG31CH.localdomain>
2026-05-12 13:27:57 -0400

    Finished link and image parsing

--- src/test_utils.py
+++ src/test_utils.py
@@ -1,5 +1,5 @@
 import unittest
-from utils import split_nodes_delimiter
+from utils import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 from textnode import TextType, TextNode
 
 class TestDelimiter(unittest.TestCase):
@@ -61,3 +61,28 @@ class TestDelimiter(unittest.TestCase):
                                   TextNode(" and ", TextType.TEXT),
                                   TextNode("italic", TextType.ITALIC),
                                   TextNode("", TextType.TEXT)])
+        
+class TestRegexForLinks(unittest.TestCase):
+    def test_regex_function_images(self):
+        test_text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        result = extract_markdown_images(test_text)
+        self.assertEqual(result, [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")])
+
+    def test_regex_function_links(self):
+        test_text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        result = extract_markdown_links(test_text)
+        self.assertEqual(result, [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")])
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_link_and_image(self):
+        test_text = "Here is a ![image](https://example.com/img.png) and a [link](https://example.com)"
+        result = extract_markdown_images(test_text)
+        result.extend(extract_markdown_links(test_text))
+        self.assertEqual(result, [("image", "https://example.com/img.png"),("link", "https://example.com")])
+
+
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 from textnode import TextType, TextNode
+import re
 
 
 def split_nodes_delimiter(old_nodes: list[TextNode], delimiter:str, text_type:TextType)->list:
@@ -18,4 +19,13 @@ def split_nodes_delimiter(old_nodes: list[TextNode], delimiter:str, text_type:Te
                     new_node = TextNode(splits[i], TextType.TEXT)
                     new_nodes.append(new_node)
     return new_nodes
+
+def extract_markdown_images(text: str)->list[tuple]:
+    result = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return result
+
+def extract_markdown_links(text: str)->list[tuple]:
+    result = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return result
+    
 
