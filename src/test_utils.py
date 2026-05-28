a8e674d48b05514e94399a4b70e8710b95b6ee10
andreas <andre@LAPTOP-61UG31CH.localdomain>
2026-05-28 10:53:15 -0400

    Finsihed text to textnodes function

--- src/test_utils.py
+++ src/test_utils.py
@@ -1,5 +1,5 @@
 import unittest
-from utils import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
+from utils import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link, text_to_textnodes
 from textnode import TextType, TextNode
 
 class TestDelimiter(unittest.TestCase):
@@ -138,3 +138,27 @@ class TestLinkandImageExtraction(unittest.TestCase):
         node = TextNode("This one is **bold**", TextType.BOLD)
         new_nodes = split_nodes_image([node])
         self.assertEqual(new_nodes, [TextNode("This one is **bold**", TextType.BOLD)])
+
+class TestTexttoTextNode(unittest.TestCase):
+    def test_multiple_types(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        result = text_to_textnodes(text)
+        self.assertEqual(result, [
+    TextNode("This is ", TextType.TEXT),
+    TextNode("text", TextType.BOLD),
+    TextNode(" with an ", TextType.TEXT),
+    TextNode("italic", TextType.ITALIC),
+    TextNode(" word and a ", TextType.TEXT),
+    TextNode("code block", TextType.CODE),
+    TextNode(" and an ", TextType.TEXT),
+    TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+    TextNode(" and a ", TextType.TEXT),
+    TextNode("link", TextType.LINK, "https://boot.dev"),
+        ])
+    def test_bold_and_italic(self):
+        text = "Here is a string with **bold text** and _italic text_"
+        result = text_to_textnodes(text)
+        self.assertEqual(result, [TextNode("Here is a string with ", TextType.TEXT),
+                                  TextNode("bold text", TextType.BOLD),
+                                  TextNode(" and ", TextType.TEXT),
+                                  TextNode("italic text", TextType.ITALIC)])
--- src/utils.py
+++ src/utils.py
@@ -68,5 +68,14 @@ def split_nodes_link(old_nodes:list[TextNode]) -> list[TextNode]:
                 new_node = TextNode(part, TextType.TEXT)
             new_nodes.append(new_node)
     return new_nodes
+
+def text_to_textnodes(text: str)->list[TextNode]:
+    node = TextNode(text, TextType.TEXT)
+    bold = split_nodes_delimiter([node], "**", TextType.BOLD)
+    italic = split_nodes_delimiter(bold, "_", TextType.ITALIC)
+    code = split_nodes_delimiter(italic, "`", TextType.CODE)
+    images = split_nodes_image(code)
+    links = split_nodes_link(images)
+    return links
     
 
