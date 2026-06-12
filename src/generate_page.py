1ecf95f5a04b2a9458f0d88d187c4a6fd5d22cdc
andreas <andre@LAPTOP-61UG31CH.localdomain>
2026-06-12 17:48:54 -0400

    Finished the functions. Now to deploy

--- src/generate_page.py
+++ src/generate_page.py
@@ -2,6 +2,8 @@ from utils import markdown_to_html_node
 from extract_title import extract_title
 from split_and_replace import split_and_replace
 from pathlib import Path
+import os
+import shutil
 
 def generate_page(from_path:str, template_path:str, dest_path:str):
     print(f"Generating page {from_path} to {dest_path} using {template_path}")
@@ -16,4 +18,16 @@ def generate_page(from_path:str, template_path:str, dest_path:str):
     dest_path = Path(dest_path)
     dest_path.parent.mkdir(parents=True, exist_ok = True)
     dest_path.write_text(full_html_page)
+
+def generate_page_recursive(dir_path_content:str, template_path:str, dest_dir_path:str):
+    for root, dirs, files in os.walk(dir_path_content):
+        rel_path = os.path.relpath(root, dir_path_content)
+        des_path = os.path.join(dest_dir_path, rel_path)
+        os.makedirs(des_path, exist_ok=True)
+        for file in files:
+            src_file = os.path.join(root, file)
+            file = file.replace(".md", ".html")
+            dest_file = os.path.join(des_path, file)
+            generate_page(src_file, template_path, dest_file)
+
     
--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from textnode import TextNode
 from copy_from_static_to_public import copy_from_static_to_public
 from generate_page import generate_page
+from generate_page import generate_page_recursive
 
 def main():
-    copy_from_static_to_public()
-    generate_page("content/index.md", "template.html", "public/index.html")
+    ##copy_from_static_to_public()
+    ##generate_page("content/index.md", "template.html", "public/index.html")
+    generate_page_recursive("/home/andre/projects/github/ako1993/staticsite/content", "template.html", "/home/andre/projects/github/ako1993/staticsite/public")
 main()
